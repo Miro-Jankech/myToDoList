cd4637abb90b6c3b250ede0f3e9f74d27e455a63
Miro Jankech <jankechmiro@gmail.com>
2025-06-10 10:36:09 +0200

    display name of the user

---
+++ src/app/dummy-users.ts
@@ -0,0 +1,32 @@
+export const DUMMY_USERS = [
+  {
+    id: 'u1',
+    name: 'Daniela Miklankova',
+    avatar: 'user-1.jpg',
+  },
+  {
+    id: 'u2',
+    name: 'Marian Jankech',
+    avatar: 'user-2.jpg',
+  },
+  {
+    id: 'u3',
+    name: 'Lubos Jankech',
+    avatar: 'user-3.jpg',
+  },
+  {
+    id: 'u4',
+    name: 'Peter Jankech',
+    avatar: 'user-4.jpg',
+  },
+  {
+    id: 'u5',
+    name: 'Michal Jankech',
+    avatar: 'user-5.jpg',
+  },
+  {
+    id: 'u6',
+    name: 'Anna Jankechova',
+    avatar: 'user-6.jpg',
+  },
+];
